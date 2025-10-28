e36c33e9e60b947d7a35cbccff3f88a86810340a
EsterAlvim03 <11221104146@alunos.umc.br>
2025-10-28 09:08:40 -0300

    fix: remove unused components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -4,4 +4,3 @@ export { default as EmptyComponent } from './EmptyComponent';
 export { default as Icon } from './Icon';
 export { default as Input } from './Input';
 export { default as Loader } from './Loader';
-export { default as Switch } from './Switch';
